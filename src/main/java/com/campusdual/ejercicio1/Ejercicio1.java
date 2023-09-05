e79a5a1cc070260a1316695a5bf71e0588c12051
braisgr <bgruibal@gmail.com>
2023-09-05 09:51:05 +0200

    Ejercicio1

--- src/main/java/com/campusdual/ejercicio1/Ejercicio1.java
+++ src/main/java/com/campusdual/ejercicio1/Ejercicio1.java
@@ -3,6 +3,6 @@ package com.campusdual.ejercicio1;
 public class Ejercicio1 {
 
     public static void main(String[] args) {
-        System.out.println("1");
+        System.out.println("Hola mundo");
     }
 }
